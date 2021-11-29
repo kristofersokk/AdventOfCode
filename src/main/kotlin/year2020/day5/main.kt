2d7d64e400043a1d3169ba7bc3c0c79289f3a623
Kristofer Sokk <krsokk@ttu.ee>
2021-11-29 16:18:26 +0200

    2020 - Day 5

---
+++ src/main/kotlin/year2020/day5/main.kt
@@ -0,0 +1,28 @@
+package year2020.day5
+
+import java.io.File
+
+fun main() {
+    val file = File("src/main/resources/2020-day5.txt")
+    val lines = file.readLines()
+    val boardingPasses = lines.map { line ->
+        val r = line.substring(0 until 7)
+            .replace('F', '0')
+            .replace('B', '1')
+            .toInt(2)
+        val c = line.substring(7)
+            .replace('L', '0')
+            .replace('R', '1')
+            .toInt(2)
+        val id = 8 * r + c
+        id
+    }.toSet()
+    println(boardingPasses.maxOf { it })
+
+    for (i in 0 until 8 * 128) {
+        if (i !in boardingPasses) {
+            println(i)
+        }
+    }
+
+}
