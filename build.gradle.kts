ee8334261f6000c544fcc620b9264f72e5d1a437
Kristofer Sokk <kristofer.sokk@gmail.com>
2025-12-02 01:23:52 +0200

    Upgrade gradle and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,21 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.2.21"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
-val kotlinVersion = "2.1.0"
-val coroutinesVersion = "1.9.0"
+val kotlinVersion = "2.2.21"
+val coroutinesVersion = "1.10.2"
+
+kotlin {
+    jvmToolchain(24)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(24))
+    }
+}
 
 repositories {
     mavenCentral()
