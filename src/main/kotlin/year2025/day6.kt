3b297f196bdd41d98bacf5cda50fd6c325030626
Kristofer Sokk <kristofer.sokk@gmail.com>
2025-12-06 20:55:25 +0200

    2025 day 6

---
+++ src/main/kotlin/year2025/day6.kt
@@ -0,0 +1,48 @@
+package year2025
+
+import java.io.File
+import java.math.BigInteger
+
+fun main() {
+    val file = File("src/main/resources/2025/2025-day6.txt")
+    val lines = file.readLines()
+
+    val numberLines = lines.dropLast(1).map { it.trim().split("\\s+".toRegex()).map { it.toBigInteger() } }
+    val operators = lines.last().trim().split("\\s+".toRegex()).map { it.first() }
+
+    val result1 = (0 until numberLines.first().size)
+        .map { x ->
+            numberLines.map { it[x] }.reduce(
+                when (operators[x]) {
+                    '+' -> BigInteger::plus
+                    else -> BigInteger::times
+                }
+            )
+        }
+        .reduce(BigInteger::plus)
+
+    println("result1: $result1")
+
+    val result2 =
+        (lines.first().length - 1 downTo 0)
+            .flatMap { x -> lines.indices.map { y -> lines[y].getOrElse(x) { ' ' } } }
+            .joinToString(separator = "")
+            .split("(?<=[+|*])\\s+".toRegex())
+            .map { problem ->
+                problem.dropLast(1)
+                    .trim()
+                    .split("\\s+".toRegex())
+                    .map { it.toBigInteger() }
+                    .reduce(
+                        when (problem.last()) {
+                            '+' -> BigInteger::plus
+                            else -> BigInteger::times
+                        }
+                    )
+            }
+            .reduce(BigInteger::plus)
+
+
+    println("result2: $result2")
+
+}
